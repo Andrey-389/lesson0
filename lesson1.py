6b6fd85aa0764f1cffad3aae09145ef21509724a
Andrey Narchev <andreinarchev@yandex.ru>
2024-07-14 15:28:24 +0300

    Add files via upload

---
+++ lesson1.py
@@ -0,0 +1,13 @@
+#1st progrqm
+print(9**0.5*5)
+#2nd program
+print(9.99>9.98 and 1000!=1000.1)
+#3rd program
+print(123%100//10)
+print(234%100//10)
+print(567%100//10)
+print(678%100//10)
+print(23+67)
+#4th program
+print(int(13)==(float(13)) or (int(42)==(float(42))))
+
